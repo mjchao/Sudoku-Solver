61074879aff5111e252e3f21e072018261634421
Mickey Chao <mjchao2@gmail.com>
2015-12-28 21:35:24 -0500

    Generate test set data for digit recognition

--- PhotoOCRTraining/src/Main.java
+++ PhotoOCRTraining/src/Main.java
@@ -11,13 +11,18 @@ public class Main {
 	public static final void main( String[] args ) throws Exception {
 		FontManager.init();
 		Generator g = new Generator();
+		
+		//set to "test" to generate test data
+		//set to "train" to generate train data
+		//set to "cv-n" to generate n-th cross-validation set 
+		String saveDir = "test";
 		for ( int digit = 0 ; digit<=9 ; ++digit ) {
 			//clear the data file for the given digit, so that we don't keep appending
 			//too much data
-			PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter( "train/" + digit + "/" + digit ) ) );
+			PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter( saveDir + "/" + digit + "/" + digit ) ) );
 			out.close();
-			BufferedImage output = g.generateN( digit , 2500 , 50 , 50 );
-			ImageIO.write( output , "png" , new File( digit + "-tiles.png" ) );
+			BufferedImage output = g.generateN( digit , 2500 , 50 , 50 , saveDir );
+			ImageIO.write( output , "png" , new File( saveDir + "/" + digit + "-tiles.png" ) );
 		}
 	}
 }
